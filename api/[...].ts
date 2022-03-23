d9b05fe8d31ac3f7c04b045a4c605c010e67e8f8
Tiago Gimenes <tlgimenes@gmail.com>
2022-03-23 13:33:07 -0300

    add procfile

--- api/[...].ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env DENO_DIR=/tmp deno run --no-check --unstable --include-files=../**/*
-
-import { getHandler } from "../packages/micro/server.ts";
-
-const handler = await getHandler({
-  root: 'examples/website'
-})
-
-export default handler
